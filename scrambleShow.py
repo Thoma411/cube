d384143bd8ca817e242c06d7b34dcc11a1e41788
Thoma411 <NADTM_Thomax11@163.com>
2023-06-09 18:20:22 +0800

    测试用例

---
+++ scrambleShow.py
@@ -0,0 +1,11 @@
+'''
+Author: Thoma411
+Date: 2023-06-09 17:55:09
+LastEditTime: 2023-06-09 18:08:30
+Description: 
+'''
+import scrambler as sm
+import moveMethod as mm
+
+s1 = sm.genScmb3(sm.SCM_LEN_MIN, sm.SCM_LEN_MAX)
+mm.multiStep(s1)
